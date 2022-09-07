969c321255a1e8eb8db099e2564799561f1a65ed
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-09-07 22:11:03 +0800

    [FEAT] add sql query condition status = 1

--- models/debt_model.js
+++ models/debt_model.js
@@ -3,7 +3,7 @@ const pool = require('../config/mysql');
 const getDebtMain = async (group, pageSize, paging) => {
     // const debtMainSql = 'SELECT id, debt_date, title, total, lender FROM debt_main WHERE gid = ? ORDER BY debt_date DESC, id ASC LIMIT ?, ?;'; //排序方式為日期+建立順序(id)
     // const [debtMainResult] = await pool.query(debtMainSql, [group, page, pageSize]);
-    const debtMainSql = 'SELECT id, debt_date, title, total, lender FROM debt_main WHERE gid = ? LIMIT ? OFFSET ?;'; //排序方式為日期+建立順序(id)
+    const debtMainSql = 'SELECT id, debt_date, title, total, lender FROM debt_main WHERE gid = ? LIMIT ? OFFSET ? AND status = 1;'; //排序方式為日期+建立順序(id)
     const debtMainResult = await pool.query(debtMainSql, [group, pageSize, pageSize * paging]);
     return debtMainResult;
 };
