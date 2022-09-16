bc40900dfbd9b80e92571992bbccea24622a5ca4
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-09-16 15:17:44 +0800

    [FEAT] add log level system

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ require('dotenv').config();
 const { PORT } = process.env;
 const port = PORT;
 
+const BG_COLOR = {
+    RED: '41',
+    GREEN: '42',
+    YELLOW: '43',
+};
+
+let log = console.log;
+function mylog() {}
+function getMyLog(color) {
+    return function () {
+        let fnName = arguments.callee.caller.name ? arguments.callee.caller.name : 'anonymous';
+        log(new Date().toISOString(), `\x1b[${color}m${fnName}\x1b[0m`, ...arguments);
+    };
+}
+console.log = getMyLog(BG_COLOR.GREEN);
+console.error = getMyLog(BG_COLOR.RED);
+console.warn = getMyLog(BG_COLOR.YELLOW);
+
 // Express Initialization
 const express = require('express');
 const userRoute = require('./routes/user_route');
