7612c7e8b71071f4bef7fc1b2668fe25b40f0eae
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-09-18 13:50:45 +0800

    [FIX] align with front-end variable

--- controllers/group_controller.js
+++ controllers/group_controller.js
@@ -6,23 +6,23 @@ const Mapping = require('../config/mapping');
 const createGroup = async (req, res) => {
     const group_name = req.body.group_name;
     const group_type = req.body.group_type;
-    const members = req.body.members;
-    console.info('req body', group_name, group_type, members);
+    const groupUsers = req.body.groupUsers;
+    console.info('req body', group_name, group_type, groupUsers);
 
     //取得MySql連線
     const conn = await pool.getConnection();
     await conn.beginTransaction();
 
     //MySql建立group
-    const groupResult = await Admin.createGroup(conn, group_name, Mapping.GROUP_TYPE[group_type], members);
+    const groupResult = await Admin.createGroup(conn, group_name, group_type, groupUsers);
     if (!groupResult) {
         await conn.rollback();
         await conn.release();
         return res.status(500).json({ err: 'Internal Server Error' });
     }
     const gid = groupResult;
-    const memberIds = members.map((member) => {
-        return member.uid;
+    const memberIds = groupUsers.map((user) => {
+        return user.uid;
     });
     console.log('to Neo:   ', gid, memberIds);
     //Neo4j建立節點
@@ -32,18 +32,6 @@ const createGroup = async (req, res) => {
     }
     conn.commit();
     res.status(200).json({ data: { gid } });
-};
-const createMember = async (req, res) => {
-    if (req.userGroupRole.gid !== req.params.gid || req.userGroupRole.role < Mapping.USER_ROLE['administer']) {
-        return res.status(403).json({ err: 'No authorization.' });
-    }
-    const groupId = req.body.gid;
-    const uid = req.body.uid;
-    const memberId = await Admin.createMember(groupId, uid, role);
-    if (!memberId) {
-        return res.status(500).json({ err: 'Internal Server Error' });
-    }
-    res.status(200).json({ data: { memberId } });
 };
 const getGroupUsers = async (req, res) => {
     console.log('@control getGroupUsers');
@@ -57,17 +45,34 @@ const getGroupUsers = async (req, res) => {
     res.status(200).json({ data: members });
 };
 const updateGroup = async (req, res) => {
-    if (req.userGroupRole.gid !== req.params.gid || req.userGroupRole.role < Mapping.USER_ROLE['administer']) {
-        return res.status(403).json({ err: 'No authorization.' });
+    try {
+        if (req.userGroupRole.gid !== Number(req.params.id) || req.userGroupRole.role < Mapping.USER_ROLE['administer']) {
+            return res.status(403).json({ err: 'No authorization.' });
+        }
+        console.log(req.body);
+        const gid = Number(req.params.id);
+        const group_name = req.body.group_name;
+        const result = await Admin.updateGroup(gid, group_name);
+        if (!result) {
+            return res.status(500).json({ err: 'Internal Server Error' });
+        }
+
+        let groupUserIds = [];
+        for (let i = 0; i < req.body.groupUsers; i++) {
+            const uid = user.uid;
+            const role = user.role;
+            const insertId = await Admin.createMember(gid, uid, role);
+            groupUserIds.push(insertId);
+            if (!memberId) {
+                return res.status(500).json({ err: 'Internal Server Error' });
+            }
+        }
+
+        res.status(200).json({ data: groupUserIds });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ err: 'Internal Server Error' });
     }
-    console.log(req.body);
-    let gid = req.body.gid;
-    let group_name = req.body.group_name;
-    result = await Admin.updateGroup(gid, group_name);
-    if (!result) {
-        return res.status(500).json({ err: 'Internal Server Error' });
-    }
-    res.status(200).json({ data: null });
 };
 const deleteMember = async (req, res) => {
     if (req.userGroupRole.gid !== req.params.gid || req.userGroupRole.role < Mapping.USER_ROLE['administer']) {
@@ -81,4 +86,19 @@ const deleteMember = async (req, res) => {
     }
     res.status(200).json({ data: null });
 };
-module.exports = { createGroup, createMember, getGroupUsers, updateGroup, deleteMember };
+
+// 併到update裡面
+// const createMember = async (req, res) => {
+//     if (req.userGroupRole.gid !== req.params.gid || req.userGroupRole.role < Mapping.USER_ROLE['administer']) {
+//         return res.status(403).json({ err: 'No authorization.' });
+//     }
+//     const groupId = req.params.gid;
+//     const uid = req.body.uid;
+//     const memberId = await Admin.createMember(groupId, uid, role);
+//     if (!memberId) {
+//         return res.status(500).json({ err: 'Internal Server Error' });
+//     }
+//     res.status(200).json({ data: { memberId } });
+// };
+
+module.exports = { createGroup, getGroupUsers, updateGroup, deleteMember };
--- routes/group_route.js
+++ routes/group_route.js
@@ -2,12 +2,11 @@ const express = require('express');
 const { createGroup, createMember, getGroupUsers, updateGroup, deleteMember } = require('../controllers/group_controller');
 const { authentication, authorization } = require('../util/auth');
 const groupRoute = express.Router();
-// groupRoute.use(authentication, authorization);
 
 groupRoute.get('/users/:id', authentication, authorization, getGroupUsers);
-groupRoute.post('/', authentication, createGroup);
-groupRoute.post('/user', authentication, authorization, createMember);
-groupRoute.put('/', authentication, authorization, updateGroup);
+groupRoute.post('/group', authentication, createGroup);
+// groupRoute.post('/user', authentication, authorization, createMember); //併到update裡面
+groupRoute.put('/group/:id', authentication, authorization, updateGroup);
 groupRoute.delete('/user/:gid/:uid', authentication, authorization, deleteMember);
 
 module.exports = groupRoute;
