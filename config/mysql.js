61433bcbecafafbe847760a896f50348df29f751
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-09-23 18:22:59 +0800

    [FEAT] change mysql to rds

--- config/mysql.js
+++ config/mysql.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv').config();
 const mysql = require('mysql2');
 
+console.log(process.env.RDS_HOST, process.env.RDS_PORT, process.env.RDS_USER, process.env.RDS_PASS, process.env.RDS_NAME);
 const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: process.env.RDS_HOST,
+    port: process.env.RDS_PORT,
+    user: process.env.RDS_USER,
+    password: process.env.RDS_PASS,
+    database: process.env.RDS_NAME,
     dateStrings: 'date',
 });
 const pool = connection.promise();
