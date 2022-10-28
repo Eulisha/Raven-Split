7fd84a0741d34548464da39640684bc2170ec1ec
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-10-28 11:36:06 +0800

    [FEAT] add stress test file

---
+++ util/stress_test.js
@@ -0,0 +1,20 @@
+const { driver, neo4j } = require('../config/neo4j');
+const GraphHandler = require('./graph_handler');
+const Graph = require('../models/graph_model');
+const pool = require('../config/mysql');
+
+const updateBestPathGraph = async (gid) => {
+    try {
+        const [graph, debtsForUpdate] = await GraphHandler.getBestxPath(txc, gid);
+        const updateGraph = await Graph.updateBestPath(txc, neo4j.int(gid), debtsForUpdate);
+        await txc.commit();
+    } catch (err) {
+        console.error(err);
+        await txc.rollback();
+        return err;
+    } finally {
+        session.close();
+    }
+};
+
+updateBestPathGraph(500);
