7f01b14e2d24d6e92497e37fc26e57e26b46ce86
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-10-28 12:13:18 +0800

    [FEAT] add session and dotenv

--- util/stress_test.js
+++ util/stress_test.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { driver, neo4j } = require('../config/neo4j');
 const GraphHandler = require('./graph_handler');
 const Graph = require('../models/graph_model');
@@ -5,6 +6,8 @@ const pool = require('../config/mysql');
 
 const updateBestPathGraph = async (gid) => {
     try {
+        const session = driver.session();
+        const txc = session.beginTransaction();
         const [graph, debtsForUpdate] = await GraphHandler.getBestxPath(txc, gid);
         const updateGraph = await Graph.updateBestPath(txc, neo4j.int(gid), debtsForUpdate);
         await txc.commit();
